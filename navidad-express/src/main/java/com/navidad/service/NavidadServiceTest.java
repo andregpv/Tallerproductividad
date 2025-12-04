b04a31ec95283b7434d2d43b06aaa715dcc5c600
andregpv <145367822+andregpv@users.noreply.github.com>
2025-12-04 01:11:55 -0600

    Prueba junit

---
+++ navidad-express/src/main/java/com/navidad/service/NavidadServiceTest.java
@@ -0,0 +1,12 @@
+package com.navidad.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class NavidadServiceTest {
+
+    @Test
+    void pruebaBasica() {
+        assertEquals(1, 80808590);
+    }
+}
